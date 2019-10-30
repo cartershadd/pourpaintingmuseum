0271460925bd83241c00775e94d3d11b56bc7b41
Carter Shadden <carter.shadden@gmail.com>
2019-10-29 21:49:43 -0400

    checks if user signs in before upload and at the personal upload page

--- src/Pages/PersonalUploads.js
+++ src/Pages/PersonalUploads.js
@@ -4,6 +4,7 @@ import {db} from "../firebase";
 import Card from "react-bootstrap/Card";
 import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
 class PersonalUploads extends Component {
     constructor(props) {
@@ -14,11 +15,27 @@ class PersonalUploads extends Component {
         }
     }
 
+    uiConfig = {
+        // Popup signin flow rather than redirect flow.
+        signInFlow: 'popup',
+        // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+        signInSuccessUrl: '/myUploads',
+        // We will display Google and Facebook as auth providers.
+        signInOptions: [
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            // firebase.auth.FacebookAuthProvider.PROVIDER_ID
+        ],
+        callbacks: {
+            // Avoid redirects after sign-in.
+            signInSuccessWithAuthResult: () => false
+        }
+    };
+
     deletePainting = (id) => {
-        db.collection("paintings").doc(id).delete().then(function() {
+        db.collection("paintings").doc(id).delete().then(function () {
             console.log("Document removed successfully!");
-        }).catch(function(error) {
-            console.error("Error removing document: ", + id, error);
+        }).catch(function (error) {
+            console.error("Error removing document: ", +id, error);
         })
     };
 
@@ -46,59 +63,70 @@ class PersonalUploads extends Component {
 
     render() {
         return (
-            <div className="painting-container">
-                {this.state.paintings.map((painting, index) => (
-                    <Card key={index}>
-                        <Card.Img variant="right" src={painting.image} className="painting-thumbnail rounded"/>
-                        <Card.Footer className="badge-wrapper">
-                            {painting.dirty ?
-                                <Badge className="badge" pill variant="dark">Dirty Pour</Badge> :
-                                <div></div>
-                            }
-                            {painting.dutch ?
-                                <Badge className="badge" pill variant="success">Dutch Pour</Badge> :
-                                <div></div>
-                            }
-                            {painting.flip ?
-                                <Badge className="badge" pill variant="info">Flip Cup</Badge> :
-                                <div></div>
-                            }
-                            {painting.floetrol ?
-                                <Badge className="badge" pill variant="primary">Floetrol</Badge> :
-                                <div></div>
-                            }
-                            {painting.colander ?
-                                <Badge className="badge" pill variant="secondary">Colander</Badge> :
-                                <div></div>
-                            }
-                            {painting.silicone ?
-                                <Badge className="badge" pill variant="info">Silicone</Badge> :
-                                <div></div>
-                            }
-                            {painting.hairdryer ?
-                                <Badge className="badge" pill variant="success">Hairdryer</Badge> :
-                                <div></div>
-                            }
-                            {painting.balloon ?
-                                <Badge className="badge" pill variant="primary">Balloon Technique</Badge> :
-                                <div></div>
-                            }
-                            {painting.liquitex ?
-                                <Badge className="badge" pill variant="dark">Liquitex</Badge> :
-                                <div></div>
-                            }
-                            {painting.blowtorch ?
-                                <Badge className="badge" pill variant="danger">Blowtorch</Badge> :
-                                <div></div>
-                            }
-                            {painting.other ?
-                                <Badge className="badge" pill variant="danger">Other</Badge> :
-                                <div></div>
-                            }
-                        </Card.Footer>
-                        <Button onClick={() => this.deletePainting(painting.id)} className="delete-button" variant="danger">Delete</Button>
-                    </Card>
-                ))}
+            <div>
+                {this.state.isSignedIn ?
+                    <div className="painting-container">
+                        {this.state.paintings.map((painting, index) => (
+                            <Card key={index}>
+                                <Card.Img variant="right" src={painting.image}
+                                          className="painting-thumbnail rounded"/>
+                                <Card.Footer className="badge-wrapper">
+                                    {painting.dirty ?
+                                        <Badge className="badge" pill variant="dark">Dirty Pour</Badge> :
+                                        <div></div>
+                                    }
+                                    {painting.dutch ?
+                                        <Badge className="badge" pill variant="success">Dutch Pour</Badge> :
+                                        <div></div>
+                                    }
+                                    {painting.flip ?
+                                        <Badge className="badge" pill variant="info">Flip Cup</Badge> :
+                                        <div></div>
+                                    }
+                                    {painting.floetrol ?
+                                        <Badge className="badge" pill variant="primary">Floetrol</Badge> :
+                                        <div></div>
+                                    }
+                                    {painting.colander ?
+                                        <Badge className="badge" pill variant="secondary">Colander</Badge> :
+                                        <div></div>
+                                    }
+                                    {painting.silicone ?
+                                        <Badge className="badge" pill variant="info">Silicone</Badge> :
+                                        <div></div>
+                                    }
+                                    {painting.hairdryer ?
+                                        <Badge className="badge" pill variant="success">Hairdryer</Badge> :
+                                        <div></div>
+                                    }
+                                    {painting.balloon ?
+                                        <Badge className="badge" pill variant="primary">Balloon Technique</Badge> :
+                                        <div></div>
+                                    }
+                                    {painting.liquitex ?
+                                        <Badge className="badge" pill variant="dark">Liquitex</Badge> :
+                                        <div></div>
+                                    }
+                                    {painting.blowtorch ?
+                                        <Badge className="badge" pill variant="danger">Blowtorch</Badge> :
+                                        <div></div>
+                                    }
+                                    {painting.other ?
+                                        <Badge className="badge" pill variant="danger">Other</Badge> :
+                                        <div></div>
+                                    }
+                                </Card.Footer>
+                                <Button onClick={() => this.deletePainting(painting.id)} className="delete-button"
+                                        variant="danger">Delete</Button>
+                            </Card>
+                        ))}
+                    </div>
+                    :
+                    <div className="login-warning">
+                        <p className="message">Please log in to view your uploads.</p>
+                        <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()} id="login"/>
+                    </div>
+                }
             </div>
         );
     }
--- src/Pages/uploadPage.js
+++ src/Pages/uploadPage.js
@@ -9,6 +9,7 @@ import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
 import firebase from "firebase";
 import shortid from 'shortid';
 import Button from "react-bootstrap/Button";
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
 registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
 
@@ -32,6 +33,22 @@ class UploadPage extends Component {
             isSignedIn: false
         }
     }
+
+    uiConfig = {
+        // Popup signin flow rather than redirect flow.
+        signInFlow: 'popup',
+        // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+        signInSuccessUrl: '/uploads',
+        // We will display Google and Facebook as auth providers.
+        signInOptions: [
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            // firebase.auth.FacebookAuthProvider.PROVIDER_ID
+        ],
+        callbacks: {
+            // Avoid redirects after sign-in.
+            signInSuccessWithAuthResult: () => false
+        }
+    };
 
     onSubmit = (e) => {
         e.preventDefault();
@@ -138,147 +155,157 @@ class UploadPage extends Component {
     };
 
     componentDidMount() {
-        firebase.auth().onAuthStateChanged(
-            (user) => {
-                this.setState({isSignedIn: !!user});
+        this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+            (user) => this.setState({isSignedIn: !!user})
+        );
+    }
 
-            });
-    };
+    // Make sure we un-register Firebase observers when the component unmounts.
+    componentWillUnmount() {
+        this.unregisterAuthObserver();
+    }
 
     render() {
         return (
-            <Form className="upload-form" onSubmit={this.onSubmit}>
-                <Form.Label className="form-label" column={1}>Upload Your Work Here!</Form.Label>
-                <FilePond
-                    ref={ref => (this.pond = ref)}
-                    files={this.state.files}
-                    allowMultiple={true}
-                    maxFiles={3}
-                    server={this.server}
-                    onupdatefiles={fileItems => {
-                        // Set currently active file objects to this.state
-                        this.setState({
-                            files: fileItems.map(fileItem => fileItem.file)
-                        });
-                    }}
-                />
-                <Form.Group title="Technique">
-                    <Form.Label column={2}>Technique</Form.Label>
-                    {['checkbox'].map(type => (
-                        <div key={`inline-${type}`} className="upload-checks">
-                            <Form.Check
-                                inline
-                                label="Dirty Pour"
-                                id="upload-radio1"
-                                onChange={this.updateInput}
-                                value={this.state.dirty}
-                                name="dirty"
-                                type="checkbox"
-                            />
-                            <Form.Check
-                                inline
-                                label="Flip Cup"
-                                id="upload-radio2"
-                                onChange={this.updateInput}
-                                value={this.state.flip}
-                                name="flip"
-                                type="checkbox"
-                            />
-                            <Form.Check
-                                inline
-                                label="Balloon Technique"
-                                id="upload-radio3"
-                                onChange={this.updateInput}
-                                value={this.state.balloon}
-                                name="balloon"
-                                type="checkbox"
-                            />
-                            <Form.Check
-                                inline
-                                label="Dutch pour"
-                                id="upload-radio4"
-                                onChange={this.updateInput}
-                                value={this.state.dutch}
-                                name="dutch"
-                                type="checkbox"
-                            />
-                            <Form.Check
-                                inline
-                                label="Colander"
-                                id="upload-radio5"
-                                onChange={this.updateInput}
-                                value={this.state.colander}
-                                name="colander"
-                                type="checkbox"
-                            />
-                            <Form.Check
-                                inline
-                                label="Hairdryer"
-                                id="upload-radio6"
-                                onChange={this.updateInput}
-                                value={this.state.hairdryer}
-                                name="hairdryer"
-                                type="checkbox"
-                            />
-                            <Form.Check
-                                inline
-                                label="Blowtorch"
-                                id="upload-radio7"
-                                onChange={this.updateInput}
-                                value={this.state.blowtorch}
-                                name="blowtorch"
-                                type="checkbox"
-                            />
-                        </div>
-                    ))}
-                </Form.Group>
+            <div>
+                {this.state.isSignedIn ?
+                    <Form className="upload-form" onSubmit={this.onSubmit}>
+                        <Form.Label className="form-label" column={1}>Upload Your Work Here!</Form.Label>
+                        <FilePond
+                            ref={ref => (this.pond = ref)}
+                            files={this.state.files}
+                            allowMultiple={false}
+                            maxFiles={1}
+                            server={this.server}
+                            onupdatefiles={fileItems => {
+                                // Set currently active file objects to this.state
+                                this.setState({
+                                    files: fileItems.map(fileItem => fileItem.file)
+                                });
+                            }}
+                        />
 
-                <Form.Group title="Add-Ons">
-                    <Form.Label className="form-label" column={2}>Additional Materials</Form.Label>
-                    {['checkbox'].map(type => (
-                        <div key={`inline-${type}`} className="upload-checks">
-                            <Form.Check
-                                inline
-                                label="Floetrol"
-                                id="upload-radio8"
-                                onChange={this.updateInput}
-                                value={this.state.floetrol}
-                                name="floetrol"
-                                type="checkbox"
-                            />
-                            <Form.Check
-                                inline
-                                label="Silicone"
-                                id="upload-radio9"
-                                onChange={this.updateInput}
-                                value={this.state.silicone}
-                                name="silicone"
-                                type="checkbox"
-                            />
-                            <Form.Check
-                                inline
-                                label="Liquitex"
-                                id="upload-radio10"
-                                onChange={this.updateInput}
-                                value={this.state.liquitex}
-                                name="liquitex"
-                                type="checkbox"
-                            />
-                            <Form.Check
-                                inline
-                                label="Other"
-                                id="upload-radio11"
-                                onChange={this.updateInput}
-                                value={this.state.other}
-                                name="other"
-                                type="checkbox"
-                            />
-                        </div>
-                    ))}
-                </Form.Group>
-                <Button disabled={!this.state.isSignedIn} className="submit-button-upload" label="Submit" type="submit" size="lg" variant="outline-warning" value="Submit">Submit</Button>
-            </Form>
-        );
+                        <Form.Group title="Technique">
+                            <Form.Label column={2}>Technique</Form.Label>
+                            <div className="upload-checks">
+                                <Form.Check
+                                    inline
+                                    label="Dirty Pour"
+                                    id="upload-radio1"
+                                    onChange={this.updateInput}
+                                    value={this.state.dirty}
+                                    name="dirty"
+                                    type="checkbox"
+                                />
+                                <Form.Check
+                                    inline
+                                    label="Flip Cup"
+                                    id="upload-radio2"
+                                    onChange={this.updateInput}
+                                    value={this.state.flip}
+                                    name="flip"
+                                    type="checkbox"
+                                />
+                                <Form.Check
+                                    inline
+                                    label="Balloon Technique"
+                                    id="upload-radio3"
+                                    onChange={this.updateInput}
+                                    value={this.state.balloon}
+                                    name="balloon"
+                                    type="checkbox"
+                                />
+                                <Form.Check
+                                    inline
+                                    label="Dutch pour"
+                                    id="upload-radio4"
+                                    onChange={this.updateInput}
+                                    value={this.state.dutch}
+                                    name="dutch"
+                                    type="checkbox"
+                                />
+                                <Form.Check
+                                    inline
+                                    label="Colander"
+                                    id="upload-radio5"
+                                    onChange={this.updateInput}
+                                    value={this.state.colander}
+                                    name="colander"
+                                    type="checkbox"
+                                />
+                                <Form.Check
+                                    inline
+                                    label="Hairdryer"
+                                    id="upload-radio6"
+                                    onChange={this.updateInput}
+                                    value={this.state.hairdryer}
+                                    name="hairdryer"
+                                    type="checkbox"
+                                />
+                                <Form.Check
+                                    inline
+                                    label="Blowtorch"
+                                    id="upload-radio7"
+                                    onChange={this.updateInput}
+                                    value={this.state.blowtorch}
+                                    name="blowtorch"
+                                    type="checkbox"
+                                />
+                            </div>
+                        </Form.Group>
+
+                        <Form.Group title="Add-Ons">
+                            <Form.Label className="form-label" column={2}>Additional Materials</Form.Label>
+                            <div className="upload-checks">
+                                <Form.Check
+                                    inline
+                                    label="Floetrol"
+                                    id="upload-radio8"
+                                    onChange={this.updateInput}
+                                    value={this.state.floetrol}
+                                    name="floetrol"
+                                    type="checkbox"
+                                />
+                                <Form.Check
+                                    inline
+                                    label="Silicone"
+                                    id="upload-radio9"
+                                    onChange={this.updateInput}
+                                    value={this.state.silicone}
+                                    name="silicone"
+                                    type="checkbox"
+                                />
+                                <Form.Check
+                                    inline
+                                    label="Liquitex"
+                                    id="upload-radio10"
+                                    onChange={this.updateInput}
+                                    value={this.state.liquitex}
+                                    name="liquitex"
+                                    type="checkbox"
+                                />
+                                <Form.Check
+                                    inline
+                                    label="Other"
+                                    id="upload-radio11"
+                                    onChange={this.updateInput}
+                                    value={this.state.other}
+                                    name="other"
+                                    type="checkbox"
+                                />
+                            </div>
+                        </Form.Group>
+                        <Button disabled={!this.state.isSignedIn} className="submit-button-upload" label="Submit"
+                                type="submit" size="lg" variant="outline-warning" value="Submit">Submit</Button>
+                    </Form>
+                    :
+                    <div className="login-warning">
+                        <p className="message">Please log in to upload paintings.</p>
+                        <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()} id="login"/>
+                    </div>
+                }
+            </div>
+        )
     }
 }
 
